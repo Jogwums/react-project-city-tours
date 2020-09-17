4f8c28bf48b196fcb13b98fa448ebbeb034dd08e
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2020-09-17 12:29:36 +0100

    second commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,9 +9,9 @@ export default class Navbar extends Component {
             <nav className="navbar">
                 <img src={logo} alt="City Tours" width="100" />
                 <ul className="nav-links"> 
-                    <li><a href="" className="nav-link">Home</a></li>
-                    <li><a href="" className="nav-link">About</a></li>
-                    <li><a href="" className="nav-link active">Tours</a></li>
+                    <li><a href="/" className="nav-link">Home</a></li>
+                    <li><a href="/" className="nav-link">About</a></li>
+                    <li><a href="/" className="nav-link active">Tours</a></li>
                 </ul>
             </nav>
         )
