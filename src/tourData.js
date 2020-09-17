480d436aa6af0628092a4f51fbd331fbb8dbddb0
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2020-09-17 12:52:59 +0100

    third commit

--- src/tourData.js
+++ src/tourData.js
@@ -5,7 +5,7 @@ export const tourData = [
     img: "./img/newyork.jpeg",
     name: "new york bridge tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Make the most of your time in the city that never sleeps on a guided tour of NYC’s top attractions. Travel by both boat (seasonal) and bus as you swing by the 9/11 Memorial, Central Park, Rockefeller Center and more. Then hop aboard a luxury boat for a sightseeing cruise past the Statue of Liberty and Ellis Island, complete with stunning views of the world-famous skyline and Brooklyn Bridge. See the highlights of New York City on a guided tour Hear informative commentary on the history and people that shaped NYC Snap pics of Lady Liberty, Ellis Island, and the city skyline from a cruise View the 9/11 Memorial at the former World Trade Center site"
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const tourData = [
     img: "./img/paris.jpeg",
     name: "paris lights tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Experience a lovely tour in Paris. A Tour of Paris in a panoramic covered bus is the ideal tour for a first-time visitor to Paris. Several departures a day leave from Port de la Bourdonnais, near the Eiffel Tower. You can choose the time to suit your schedule while in Paris."
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const tourData = [
     img: "./img/london.jpeg",
     name: "london royal palace tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Discover the grandeur of London aboard the open-top, double-decker Big Bus on this hop-on hop-off sightseeing tour. With your 1-, 2-, or 3-day ticket, design a personal itinerary with stops including Buckingham Palace, Hyde Park, St. Paul’s Cathedral and more. Enjoy unobstructed views and onboard commentary as you view the city’s highlights and upgrade to The Big London Day Out option to get an aerial view of the city aboard the London Eye giant observation wheel. Hop off to enjoy a free walking tour and a Thames River cruise."
   },
   {
     id: 4,
@@ -29,6 +29,6 @@ export const tourData = [
     img: "./img/tokyo.jpeg",
     name: "tokyo sushi tour",
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Enhance your experience of Nikko National Park on an enriching full-day excursion from Tokyo. Feast your eyes upon breathtaking countryside, sacred shrines, and stunning temples as you explore the region with an accomplished guide. Visit Toshogu Shrine, a UNESCO World Heritage site dedicated to Shinto worship, and hear about the structure’s fascinating history. Opt for the upgrade to pleasure your palate with a traditional Japanese lunch, and then absorb the natural splendor of Lake Chuzenji and Kegon Falls. Plus, enjoy the convenience of round-trip transportation from Tokyo on this comprehensive tour." 
   }
 ];
